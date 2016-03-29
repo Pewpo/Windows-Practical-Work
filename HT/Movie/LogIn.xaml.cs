fa1bddee3c14bb52669c84dc7bdb50276e79ef3d
Pewpo <samulir@windowslive.com>
2016-03-29 22:13:15 +0300

    Added files via upload

---
+++ HT/Movie/LogIn.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Movie.BL;
+
+namespace Movie.Menu
+{
+    /// <summary>
+    /// Interaction logic for LogIn.xaml
+    /// </summary>
+    public partial class LogIn : UserControl, ISwitchable
+    {
+        public LogIn()
+        {
+            InitializeComponent();
+        }
+
+        public void UtilizeState(object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new Mainmenu());
+        }
+    }
+}
---
+++ HT/Movie/Mainmenu.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Movie.BL;
+
+namespace Movie.Menu
+{
+    /// <summary>
+    /// Interaction logic for Mainmenu.xaml
+    /// </summary>
+    public partial class Mainmenu : UserControl, ISwitchable
+    {
+        public Mainmenu()
+        {
+            InitializeComponent();
+            IniMyStuff();
+        }
+        public void IniMyStuff()
+        {
+            mediaElement.Source = new Uri("D:\\Koulu\\windows-harkka\\Coffeemaker.mp4");
+            mediaElement.Play();
+        }
+
+        public void UtilizeState(object state)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void btnSearchMovies_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new MovieSearch());
+        }
+
+        private void btnMovieManager_Click(object sender, RoutedEventArgs e)
+        {
+            Switcher.Switch(new MovieManager());
+        }
+    }
+}
